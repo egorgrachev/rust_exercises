77e45c9980d59578b29659d801fd66743f66fa8d
egorgrachev <egorgrachev.msu@gmail.com>
2021-10-09 11:26:18 +0100

    simple vec experiment

--- rust_exercises/src/main.rs
+++ rust_exercises/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut result = vec![1,2,3];
+    let input = vec![4,5,6];
+    result.extend(input.iter());
+
+    println!("{:?}", result);
+    println!("{:?}", input);
+
 }
 
