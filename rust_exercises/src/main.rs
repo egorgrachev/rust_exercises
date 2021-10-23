b99c54bd7485c57ce025a6146f2c68fff1dfeb75
egorgrachev <egorgrachev.msu@gmail.com>
2021-10-23 14:43:03 +0100

    last element in array

--- rust_exercises/src/main.rs
+++ rust_exercises/src/main.rs
@@ -1,19 +1,12 @@
+use itertools::izip;
 use std::ops::Add;
 
-use itertools::izip;
-
 fn main() {
-    let mut result = vec![1, 2, 3];
+    let result = vec![1, 2, 3];
     let input = vec![4, 5, 6];
-    // result.extend(input.iter());
 
-    // for x in input.into_iter() {
-    //     result.push(x);
-    // }
-    // println!("{:?}", result);
-    // // println!("{:?}", input);
-
-    let output: Vec<i32> = izip!(result, input).map(|(x, y)| add(x, y)).collect();
+    let output: Vec<i32> = izip!(result,
+        input).map(|(x, y)| add(x, y)).collect();
     println!("{:?}", output);
 }
 
--- rust_exercises/tests/integration_test.rs
+++ rust_exercises/tests/integration_test.rs
@@ -1,4 +1,21 @@
 #[test]
 fn it_adds_two() {
-    assert_eq!(4, 2+2);
+    assert_eq!(4, 2 + 2);
+}
+
+
+#[test]
+fn test_last_element_in_array() {
+    let array = [1, 2, 3];
+    assert_eq!(get_last_element_in_array(&array), 3);
+
+    let empty_array = [];
+    assert_eq!(get_last_element_in_array(&empty_array), 0);
+}
+
+fn get_last_element_in_array(array: &[i32]) -> i32 {
+    if array.len() == 0 {
+        return 0
+    }
+    array.last().unwrap().clone()
 }
