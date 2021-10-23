166892fa503dca21ba2920400237c86c5e9ba45a
egorgrachev <egorgrachev.msu@gmail.com>
2021-10-23 15:26:53 +0100

    i32 -> T: Clone

--- rust_exercises/src/lib.rs
+++ rust_exercises/src/lib.rs
@@ -2,14 +2,14 @@ pub fn add(x: i32, y: i32) -> i32 {
     x + y
 }
 
-pub fn get_last_element_in_array(array: &[i32]) -> Option<i32> {
+pub fn get_last_element_in_array<T: Copy>(array: &[T]) -> Option<T> {
     match array.last() {
         Some(&e) => Some(e.clone()),
         None => None,
     }
 }
 
-pub fn get_first_element_in_array(array: &[i32]) -> Option<i32> {
+pub fn get_first_element_in_array<T: Copy>(array: &[T]) -> Option<T> {
     match array.first() {
         Some(&e) => Some(e.clone()),
         None => None,
--- rust_exercises/tests/integration_test.rs
+++ rust_exercises/tests/integration_test.rs
@@ -12,7 +12,7 @@ fn test_last_element_in_array() {
     let array = [1, 2, 3];
     assert_eq!(get_last_element_in_array(&array), Some(3));
 
-    let empty_array = [];
+    let empty_array:[i32; 0] = [];
     assert_eq!(get_last_element_in_array(&empty_array), None);
 }
 
@@ -21,6 +21,15 @@ fn test_first_element_in_array() {
     let array = [1, 2, 3];
     assert_eq!(get_first_element_in_array(&array), Some(1));
 
-    let empty_array = [];
+    let empty_array:[i32; 0] = [];
+    assert_eq!(get_first_element_in_array(&empty_array), None);
+}
+
+#[test]
+fn test_first_element_in_vector() {
+    let array = vec![1, 2, 3];
+    assert_eq!(get_first_element_in_array(&array), Some(1));
+
+    let empty_array: Vec<i32> = vec![];
     assert_eq!(get_first_element_in_array(&empty_array), None);
 }
